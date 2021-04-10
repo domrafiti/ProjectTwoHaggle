0e0ecb4bf57db69d7991c47368f5acd22194c34b
B.A. Huisken <brice.huisken@gmail.com>
2021-04-10 10:24:54 -0600

    Seeds work, for now

--- models/index.js
+++ models/index.js
@@ -3,26 +3,25 @@ const Listing = require('./Listing');
 const Category = require('./Category');
 const Status = require('./Status');
 
-
 User.hasMany(Listing, {
   foreignKey: 'user_id',
-  onDelete: 'CASCADE'
+  onDelete: 'CASCADE',
 });
 
 Listing.belongsTo(User, {
-  foreignKey: 'user_id'
+  foreignKey: 'user_id',
 });
 
-Listing.hasOne(Status, {
-  foreignKey: 'listing_id'
-});
+// Listing.hasOne(Status, {
+//   foreignKey: 'listing_id'
+// });
 
-Listing.hasMany(Category, {
-  foreignKey: 'listing_id'
-});
+// Listing.hasMany(Category, {
+//   foreignKey: 'listing_id'
+// });
 
-Category.hasMany(Listing, {
-foreignKey: 'category_id'
-});
+// Category.hasMany(Listing, {
+// foreignKey: 'category_id'
+// });
 
 module.exports = { User, Listing, Category, Status };
