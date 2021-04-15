cbf3e66a28ecdc552264124682ffc8396615c511
KB Desktop <Keithblack4290gmail.com>
2021-04-14 18:39:40 -0600

    styling and branding

---
+++ psuedo.js
@@ -0,0 +1,50 @@
+/*
+1. User comes to home page and is presented with:
+    A. Listings
+    B. Login/Sign-up
+2. Login/Sign-up
+    A. Login: (reusing code)
+        a. get information entered from the user
+        b. check it against the users_db for accuracy
+        c. login or provide error
+    B. Sign-up: (reusing code)
+        a. get information entered form the user
+        b. create the new user in the users_db
+        c. send email confiming user ID
+        c. log user in
+3. Listings/Browsing - login status doesnt matter
+    A. Continuous Scroll/Pages
+    B. Present Listings
+        a. using cards of sorts to display (routes to listing)
+            i. Item/Service Title
+            ii. Short descriptoin
+            iii. looking for
+            iv. picture (use position 0 and let poster know first image is listing image)
+    C. Filters?
+        a. categories
+        b. services OR items
+        c. one user
+    D. Sort
+    E. Top Traders - activity within last 30 days or based off rating
+    F. New/Most Recently posted
+4. Listing/One
+    A. include listing information (photos, details, looking for, description, etc.)
+    B. Check login status:
+        YES
+            a. i'm interested - contact to trade
+                i. workflow
+        NO
+            a. Prompt to login - redirect
+    C. OWNER:
+        a. unlock page to edit
+5. Profile/Listing Management
+    A. include personal information (photo, details/bio, reviews/ratings/grades)
+    B. View My Listings - link to filtered listings page
+    C. spotlight listings {optional}
+    D. Create new Listing
+*/
+
+
+
+
+
